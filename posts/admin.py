c49c7400b286e7f666deb586fa01a8014e4f74ae
zbari023 <barry450643@gmail.com>
2023-10-05 22:57:20 +0200

    add the settings app and the front-end files

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Image
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['Name', 'image']
+    list_filter = ['Name', 'image']
+    search_fields = ['Name', 'image']
+    
+    
+admin.site.register(Image ,PostAdmin)
