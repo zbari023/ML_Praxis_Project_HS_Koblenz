fc4e36d357d06b9acdb34ba39b5a1eb1a6836f90
zbari023 <barry450643@gmail.com>
2023-10-09 23:26:34 +0200

    craeting a image upload area in one section and connected it in db

--- settings/forms.py
+++ settings/forms.py
@@ -1,8 +1,13 @@
 
 from django import forms 
-from .models import UserData
+from .models import UserData , Image
 
 class NameuploadForm(forms.ModelForm):
     class Meta:
         model = UserData
         fields = ['name','email','phone']
+        
+class ImageUploadForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['image']
--- settings/views.py
+++ settings/views.py
@@ -1,6 +1,6 @@
 
 from django.shortcuts import render, redirect
-from .forms import NameuploadForm
+from .forms import NameuploadForm, ImageUploadForm
 
 # Create your views here.
 
@@ -9,9 +9,14 @@ def home(request):
     
     if request.method == 'POST':
         form = NameuploadForm(request.POST)
+        imgform = ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('/')  # Redirect to a page displaying uploaded images
+        if imgform.is_valid():
+            imgform.save()
+            return redirect('/') 
     else:
         form = NameuploadForm()
-    return render(request,'settings/base.html',{'form': form})
+        imgform = ImageUploadForm()
+    return render(request,'settings/base.html',{'form': form,'imgform': imgform})
