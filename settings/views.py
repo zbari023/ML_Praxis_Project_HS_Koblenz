9387c02dc80c76c72ddbeaae680025e5d72ca752
zbari023 <barry450643@gmail.com>
2023-10-10 20:45:02 +0200

    display the uploded image in html

--- settings/views.py
+++ settings/views.py
@@ -1,11 +1,17 @@
 
 from django.shortcuts import render, redirect
 from .forms import NameuploadForm, ImageUploadForm
+from .models import Image
 
 # Create your views here.
 
 
 def home(request):
+    if Image.objects.exists():
+        image = Image.objects.latest('id')
+        context = {'image': image}
+    else:
+        context = {'image': None}
     
     if request.method == 'POST':
         form = NameuploadForm(request.POST)
@@ -19,4 +25,6 @@ def home(request):
     else:
         form = NameuploadForm()
         imgform = ImageUploadForm()
-    return render(request,'settings/base.html',{'form': form,'imgform': imgform})
+        
+    context.update({'form': form, 'imgform': imgform})
+    return render(request, 'settings/base.html', context)
