22abeaaaca5942e9680f5da465570890b1967982
zbari023 <barry450643@gmail.com>
2023-10-27 20:41:52 +0200

    make the data-form on 6 size and responsive

--- settings/forms.py
+++ settings/forms.py
@@ -6,7 +6,7 @@ class NameuploadForm(forms.ModelForm):
     class Meta:
         model = UserData
         fields = ['name','email','phone']
-        
+
 class ImageUploadForm(forms.ModelForm):
     class Meta:
         model = Image
