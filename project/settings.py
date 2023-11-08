52dcb796548db1d1abe2c972c30fa87191447602
zbari023 <barry450643@gmail.com>
2023-11-08 19:26:10 +0100

    deployment

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,7 +120,7 @@ USE_TZ = True
 # Media-Einstellungen
 STATIC_URL = '/static/'             
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
 MEDIA_URL = '/media/'               
 MEDIA_ROOT = BASE_DIR / "media"     
